7bca202e8d19072af269c00cca3bf1116846bc1e
selvendranks <selvendranks@gmail.com>
2023-04-11 11:24:11 +0530

    latest

--- src/App.test.js
+++ src/App.test.js
@@ -7,15 +7,3 @@ test('renders learn react link', () => {
   expect(linkElement).toBeInTheDocument();
 });
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
-
-
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
